0b03131315093093721e58fb707df9dd12de9ddf
서보성 <sbosung0610@gmail.com>
2025-08-05 17:35:42 +0900

    admin.py 생성, urls.py 수정, settings.py 수정

--- indev_project/indev_project/settings.py
+++ indev_project/indev_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "missions",
     "route",
     # Basic App
+    "rest_framework",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

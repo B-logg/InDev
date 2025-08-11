1b490243e470f5d2bea62a5b085014aada2e552c
bluelion10 <01cw@naver.com>
2025-08-11 15:53:30 +0900

    feat: 미션생성 및 고객별 랜덤 미션배정 api 구현

--- indev_project/missions/models.py
+++ indev_project/missions/models.py
@@ -1,3 +1,23 @@
+# mission/models.py
 from django.db import models
+from customer.models import Customer
+from store.models import Store
 
-# Create your models here.
+class ServiceMission(models.Model):
+    content = models.TextField()
+    is_active = models.BooleanField(default=True)
+    customer = models.ForeignKey(Customer, null=True, blank=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"[Service] {self.content[:20]}"
+
+class OwnerMission(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE,null=True,blank=True)  # 어떤 가게의 미션인지
+    content = models.TextField()
+    is_active = models.BooleanField(default=True)
+    customer = models.ForeignKey(Customer, null=True, blank=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"[Owner] {self.store} - {self.content[:20]}"
--- indev_project/store/models.py
+++ indev_project/store/models.py
@@ -1,3 +1,11 @@
+# store/models.py
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# missions 테스트를 위해 store_id 가 필요해서 임시로 만든 model입니다
+# 나중에 필요하시면 바꾸셔도 상관없습니다!

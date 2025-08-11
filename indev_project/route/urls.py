df32d0aaa116751b2ec7fd9e8ad8bfdde1779e05
서보성 <sbosung0610@gmail.com>
2025-08-11 16:37:09 +0900

    urls.py 수정

--- indev_project/route/urls.py
+++ indev_project/route/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import RoutineView, VisitRoutineView
 
 urlpatterns = [
-    path('routine/', RoutineView.as_view()), # 전체 조회, 생성
-    path('routine/<int:pk>/', RoutineView.as_view()), # 특정 조회, 수정, 삭제
-    path('visitroutine/', VisitRoutineView.as_view()), # 전체 조회, 생성
-    path('visitroutine/<int:pk>/', VisitRoutineView.as_view()), # 특정 조회, 수정, 삭제
+    path('routine', RoutineView.as_view()), # 전체 조회, 생성
+    path('routine/<int:pk>', RoutineView.as_view()), # 특정 조회, 수정, 삭제
+    path('visitroutine', VisitRoutineView.as_view()), # 전체 조회, 생성
+    path('visitroutine/<int:pk>', VisitRoutineView.as_view()), # 특정 조회, 수정, 삭제
     
 ]

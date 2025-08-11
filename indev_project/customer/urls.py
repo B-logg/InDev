3f761397e3481fcce1d376ab0e63002567f4ac15
서보성 <sbosung0610@gmail.com>
2025-08-11 17:41:38 +0900

    visitroutine의 index_order 속성 자동 생성되도록 수정

--- indev_project/customer/urls.py
+++ indev_project/customer/urls.py
@@ -3,5 +3,5 @@ from .views import CustomerView
 
 urlpatterns = [
     path("", CustomerView.as_view()),          # /customer/  (GET, POST)
-    path("<int:pk>", CustomerView.as_view()), # /customer/1/ (GET, PUT, DELETE)
+    path("<int:pk>/", CustomerView.as_view()), # /customer/1/ (GET, PUT, DELETE)
 ]
--- indev_project/post/urls.py
+++ indev_project/post/urls.py
@@ -4,6 +4,6 @@ from .views import PostMassilView
 
 urlpatterns = [
     path('', PostMassilView.as_view()), # 전체 조회, 생성
-    path('<int:pk>', PostMassilView.as_view()), # 특정 조회, 수정, 삭제
+    path('<int:pk>/', PostMassilView.as_view()), # 특정 조회, 수정, 삭제
     
 ] 

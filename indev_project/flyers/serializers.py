e276f86512a998e34d82fadb111c4ffa375eb098
서보성 <sbosung0610@gmail.com>
2025-08-13 02:10:02 +0900

    flyer 모델 2차 수정

--- indev_project/flyers/serializers.py
+++ indev_project/flyers/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 
+
+BAN_WORDS = {
+    "nsfw","nude","naked","porn","explicit","sex","gore","blood","violent",
+    # 원하는대로 수정 가능
+}
+
 class GenerateFlyerSerializer(serializers.Serializer):
     prompt = serializers.CharField()
     negative_prompt = serializers.CharField(required=False, allow_blank=True, default="")
@@ -16,3 +22,9 @@ class GenerateFlyerSerializer(serializers.Serializer):
     # (선택)guidance_scale: CFG 스케일(프롬프트를 얼마나 강하게 믿을지)
     # (선택)seed: 난수 시드(같은 모델/프롬프트/파라미터라면 같은 시드 = 동일 결과)
     # 다른 결과를 보고 싶으면 시드를 바꾸거나 비우기
+
+    def validate_prompt(self, v):
+        low = v.lower()
+        if any(w in low for w in BAN_WORDS):
+            raise serializers.ValidationError("허용되지 않은 내용이 포함되어있습니다. 프롬프트를 수정하세요!")
+        return v

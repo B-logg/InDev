2f98c513fd6f4604c4efea8e4e67b3babf0fc651
서보성 <sbosung0610@gmail.com>
2025-08-24 16:55:42 +0900

    asdf

--- indev_project/post/serializers.py
+++ indev_project/post/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 from .models import PostMassil
 from route.serializers import RoutineSerializer
 from customer.serializers import CustomerSerializer
+from customer.models import Customer
 
 class PostMassilSerializer(serializers.ModelSerializer):
     routine = RoutineSerializer(read_only=True)
-    customer_id = serializers.IntegerField(source="customer.id", read_only=True)
+    customer_id = serializers.PrimaryKeyRelatedField(source="customer", queryset=Customer.objects.all() ,write_only=True)
     customer_name = serializers.CharField(source="customer.nickname", read_only=True)
     customer_character = serializers.IntegerField(source="customer.character", read_only=True)
 
     class Meta:
         model = PostMassil
         fields = '__all__'
-        read_only_fields = ["customer"]

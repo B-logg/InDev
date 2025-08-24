a34804a29ca54cc3a85f25a84edec864efbab3bc
서보성 <sbosung0610@gmail.com>
2025-08-24 11:27:11 +0900

    category 추가

--- indev_project/category/admin.py
+++ indev_project/category/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("category_id", "name")  # 원하는 필드 표시

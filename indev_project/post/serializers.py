6df6478174c917a84ee119374860e7a5439b3266
서보성 <sbosung0610@gmail.com>
2025-08-25 19:06:50 +0900

    post하는 사람 점주 추가

--- indev_project/post/serializers.py
+++ indev_project/post/serializers.py
@@ -3,12 +3,25 @@ from .models import PostMassil
 from route.serializers import RoutineSerializer
 from customer.serializers import CustomerSerializer
 from customer.models import Customer
+from store.models import Store
+
 
 class PostMassilSerializer(serializers.ModelSerializer):
     routine = RoutineSerializer(read_only=True)
+
+    # 고객 정보
     customer_id = serializers.PrimaryKeyRelatedField(source="customer", queryset=Customer.objects.all() ,write_only=True)
     customer_name = serializers.CharField(source="customer.nickname", read_only=True)
     customer_character = serializers.IntegerField(source="customer.character.character_id", read_only=True)
+
+    # 점주/가게 정보
+    store_id = serializers.PrimaryKeyRelatedField(
+        source="store", queryset=Store.objects.all(),
+        write_only=True, required=False
+    )
+    store_name = serializers.CharField(source="store.store_name", read_only=True)
+
+
 
     class Meta:
         model = PostMassil
--- indev_project/post/views.py
+++ indev_project/post/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+# post/views.py
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import PostMassil
 from .serializers import PostMassilSerializer
-from customer.models import Customer
 
-# CRUD
 class PostMassilView(APIView):
-    # 조회
+    # 전체 조회 & 특정 조회
     def get(self, request, pk=None):
         if pk:
             post = get_object_or_404(PostMassil, pk=pk)
@@ -17,26 +16,24 @@ class PostMassilView(APIView):
             posts = PostMassil.objects.all().order_by("-created_at")
             serializer = PostMassilSerializer(posts, many=True)
         return Response(serializer.data)
-    
+
     # 생성
     def post(self, request):
-        customer_id = request.data.get("customer_id")
-        customer = get_object_or_404(Customer, pk=customer_id)
         serializer = PostMassilSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(customer=customer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            post = serializer.save()
+            return Response(PostMassilSerializer(post).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     # 수정
     def put(self, request, pk):
         post = get_object_or_404(PostMassil, pk=pk)
         serializer = PostMassilSerializer(post, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            post = serializer.save()
+            return Response(PostMassilSerializer(post).data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     # 삭제
     def delete(self, request, pk):
         post = get_object_or_404(PostMassil, pk=pk)
